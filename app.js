0c9489b365accb51834915f3899766172248926c
Ali Shalabi <alishalabi@Alis-MacBook-Pro.local>
2018-08-30 10:31:32 -0700

    Users can see all reviews

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.render('home', { msg: "Hello World!" });
-})
-
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
 })
@@ -13,3 +9,14 @@ var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// This will create a mock array for us to test with
+let reviews = [
+  { title: "Outstanding Review Ali!! Good job" },
+  { title: "Next Review"}
+]
+
+//This will render our index
+ app.get('/', (req, res) => {
+   res.render('reviews-index', { reviews: reviews });
+})
