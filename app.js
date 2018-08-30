f6cc92278612cb322c518c063afe8f20f0718fe8
Ali Shalabi <alishalabi@Alis-MacBook-Pro.local>
2018-08-30 11:49:47 -0700

    Added MongoDB and Review mofel

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
+
+const Review = mongoose.model('Review', {
+  title: String
+});
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
@@ -10,13 +16,20 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-// This will create a mock array for us to test with
-let reviews = [
-  { title: "Outstanding Review Ali!! Good job" },
-  { title: "Next Review"}
-]
-
 //This will render our index
  app.get('/', (req, res) => {
-   res.render('reviews-index', { reviews: reviews });
+   Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
+
+// This will create a mock array for us to test with
+// let reviews = [
+//   { title: "Outstanding Review Ali!! Good job" },
+//   { title: "Next Review" },
+//   { title: "Additional Review" }
+// ]
