b17e2d15ac25b9823a74a10d9b0eb114d24b2102
Ali Shalabi <alishalabi@Alis-MacBook-Pro.local>
2018-09-01 13:38:21 -0700

    Users can create reviews

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+const bodyParser = require('body-parser');
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const Review = mongoose.model('Review', {
-  title: String
+  title: String,
+  description: String,
+  movieTitle: String
 });
 
 app.listen(3000, () => {
@@ -16,7 +21,7 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-//This will render our index
+//Action: Index
  app.get('/', (req, res) => {
    Review.find()
     .then(reviews => {
@@ -25,6 +30,21 @@ app.set('view engine', 'handlebars');
     .catch(err => {
       console.log(err);
     })
+})
+
+// Action: New
+app.get('/reviews/new', (req, res) => {
+    res.render('reviews-new', {});
+})
+
+//Action: Create
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((reviews) => {
+      console.log(reviews);
+      res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
 })
 
 // This will create a mock array for us to test with
