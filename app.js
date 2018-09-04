2fcf7c6d8beb2d57332f699472cbb771604a661a
Ali Shalabi <alishalabi@Alis-MacBook-Pro.local>
2018-09-03 17:21:21 -0700

    User can destroy reviews. Fixed DELETE method bug

--- app.js
+++ app.js
@@ -96,3 +96,13 @@ app.put('/reviews/:id', (req, res) => {
       console.log(err.message)
     })
 })
+
+//Action: Delete
+app.delete('/reviews/:id', function (req, res) {
+  console.log("DELETE review")
+  Review.findByIdAndRemove(req.params.id).then((review) => {
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
